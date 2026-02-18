41c02547a577ec1b052c7cf4276f0c89cf60d659
icynvm <67270766+icynvm@users.noreply.github.com>
2026-02-18 17:50:36 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -162,7 +162,7 @@ const DashboardPage: React.FC<any> = ({ items, logs, onSelectItem, onAddEquipmen
               </div>
             )}
           </div>
-          <h1 className="text-5xl md:text-7xl font-extrabold text-white tracking-tighter uppercase leading-none">Fleet</h1>
+          <h1 className="text-5xl md:text-7xl font-extrabold text-white tracking-tighter uppercase leading-none">ASSETS LIST</h1>
         </div>
         <div className="flex gap-3 w-full md:w-auto">
           <button onClick={onRefresh} className={`p-5 glass text-slate-400 hover:text-white rounded-2xl transition-all shadow-xl ${isSyncing ? 'animate-spin' : ''}`}><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" /></svg></button>
